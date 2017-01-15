1829dc59268d69102949a0dcee973a2bc88fcea6
qeesung <1245712564@qq.com>
2017-01-15 23:40:13 +0800

    add pagination to show available packages

--- src/routes/Home/components/HomeView.js
+++ src/routes/Home/components/HomeView.js
@@ -1,15 +1,9 @@
 import React from 'react'
-import DuckImage from '../assets/Duck.jpg'
 import './HomeView.scss'
+import MatrixPkgMultiPage from '../../../components/MatrixPkgMultiPage'
 
 export const HomeView = () => (
-  <div>
-    <h4>Welcome!</h4>
-    <img
-      alt='This is a duck, because Redux!'
-      className='duck'
-      src={DuckImage} />
-  </div>
+  <MatrixPkgMultiPage></MatrixPkgMultiPage>
 )
 
 export default HomeView
