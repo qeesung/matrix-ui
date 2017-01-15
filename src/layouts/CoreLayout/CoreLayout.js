ec01a956e882202dfebc9b5894062141c497ad3c
qeesung <1245712564@qq.com>
2017-01-15 16:11:26 +0800

    add sider bar to core layout

--- src/layouts/CoreLayout/CoreLayout.js
+++ src/layouts/CoreLayout/CoreLayout.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import Header from '../../components/Header'
+import SiderBar from '../../components/SiderBar'
 import './CoreLayout.scss'
 import '../../styles/core.scss'
 
 export const CoreLayout = ({ children }) => (
   <div className='container text-center'>
-    <Header />
+    <SiderBar></SiderBar>
     <div className='core-layout__viewport'>
       {children}
     </div>
